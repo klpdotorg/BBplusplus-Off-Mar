5335224181c9c01ce182613640ae15a6c53ef11e
sandhya <sandhyaramachandra24@gmail.com>
2024-01-09 17:28:58 +0530

    new version updating

--- www/js/registrationAndLogin/appLoginEditScreen.js
+++ www/js/registrationAndLogin/appLoginEditScreen.js
@@ -38,8 +38,8 @@ Game.appLoginEditScreen.prototype = {
 		_this.basePath = cordova.file.externalRootDirectory + "Android/data/com.akshara.BBplusplusOffMar/Files/Download/.gameFilesBB++V10_2/www/";
 
 		//* nEED to change this ****
-		_this.fullSize = 324454261;//,302978124
-		//BBplusplusOffMar //136,781,929 + 18,76,72,332 marathi
+		_this.fullSize = 324463447;//,302978124
+		//BBplusplusOffMar //136,781,929 + 18,76,72,332 marathi,  //95,186,236
 
 		_this.zipFiles = [["Assets1.zip", 981561], ["Assets2.zip", 87770921], ["Assets4.zip", 95177050], ["MarathiquestionSounds.zip", 136781929 ],["sounds.zip", 3742800]];// old = 3683747
 		 //["HindiquestionSounds.zip", 29272691],["KannadaquestionSounds.zip", 29272691], ["OdiyaquestionSounds.zip", 48682701], ["MarathiquestionSounds.zip", 39766797], ["TamilquestionSounds.zip", 30783494],
